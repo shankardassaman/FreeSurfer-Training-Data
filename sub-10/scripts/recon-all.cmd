520517de470f06967a479eaad85907dab25db3ed
shankardassaman <shankardassaman@gmail.com>
2017-07-18 15:22:33 -0400

    final files: qcache outputs for sub-10

--- sub-10/scripts/recon-all.cmd
+++ sub-10/scripts/recon-all.cmd
@@ -447,3 +447,7 @@ INFO: Creating symlink to fsaverage subject...
 \n mris_label2annot --s sub-10 --hemi rh --ctab /Volumes/CFMI-CFS/opt/fs6/average/colortable_BA.txt --l rh.BA1_exvivo.thresh.label --l rh.BA2_exvivo.thresh.label --l rh.BA3a_exvivo.thresh.label --l rh.BA3b_exvivo.thresh.label --l rh.BA4a_exvivo.thresh.label --l rh.BA4p_exvivo.thresh.label --l rh.BA6_exvivo.thresh.label --l rh.BA44_exvivo.thresh.label --l rh.BA45_exvivo.thresh.label --l rh.V1_exvivo.thresh.label --l rh.V2_exvivo.thresh.label --l rh.MT_exvivo.thresh.label --l rh.entorhinal_exvivo.thresh.label --l rh.perirhinal_exvivo.thresh.label --a BA_exvivo.thresh --maxstatwinner --noverbose \n
 \n mris_anatomical_stats -th3 -mgz -f ../stats/rh.BA_exvivo.stats -b -a ./rh.BA_exvivo.annot -c ./BA_exvivo.ctab sub-10 rh white \n
 \n mris_anatomical_stats -th3 -mgz -f ../stats/rh.BA_exvivo.thresh.stats -b -a ./rh.BA_exvivo.thresh.annot -c ./BA_exvivo.thresh.ctab sub-10 rh white \n
+\n\n#---------------------------------
+# New invocation of recon-all Tue Jul 18 15:22:32 EDT 2017 
+#--------------------------------------------
+#@# MotionCor Tue Jul 18 15:22:32 EDT 2017
